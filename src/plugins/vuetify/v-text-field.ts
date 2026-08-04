d8f7e437a276cb4896333cc1d887b343dd8a024c
Tapash Paul <31130310+tapash22@users.noreply.github.com>
2026-08-04 23:28:28 +0600

    Revert "Prod"

--- src/plugins/vuetify/v-text-field.ts
+++ src/plugins/vuetify/v-text-field.ts
@@ -1,17 +1,11 @@
 export const aliases: Record<string, unknown> = {};
 
 const defaultTextField = {
-  // variant: 'outlined',
-  // slim: true,
-  // hideDetails: 'auto',
-  // rounded: 'lg',
-  // bgColor: 'surface',
   variant: 'outlined',
   slim: true,
   hideDetails: 'auto',
   rounded: 'lg',
   bgColor: 'surface',
-  class: 'custom-text-field', // Add custom class here
 };
 
 export const defaults = {
